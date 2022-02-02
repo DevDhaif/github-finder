e7296d89c3f50eb6ce2c993e7e27bb27a46c73d8
DevDhaif <devdhaif@gmail.com>
2022-02-02 22:31:59 +0200

    created userResults and added it to Home

--- tailwind.config.js
+++ tailwind.config.js
@@ -35,31 +35,34 @@ module.exports = {
         animate: {
           "0%": {
             opacity: "0",
-            "stroke-dasharray": 0,
+            "stroke-dasharray": 200,
           },
           "25%": {
             opacity: "10",
-            "stroke-dasharray": 250,
+            "stroke-dasharray": 400,
           },
           "50%": {
-            opacity: " 0",
-            "stroke-dasharray": 500,
+            opacity: "10",
+            "stroke-dasharray": 600,
           },
           "75%": {
             opacity: " 10",
-            "stroke-dasharray": 750,
+            "stroke-dasharray": 800,
           },
           "100%": {
-            opacity: "100",
+            opacity: "10",
             "stroke-dasharray": 1000,
           },
         },
       },
       animation: {
         wiggle: "wiggle 1s ease-in-out infinite",
-        animate:"animate 2s ease-in-out "
+        animate: "animate 1.2s linear  infinite",
       },
       // animation: { animate: "animate 2s cubic-besizer(0,0.23,1,0.1)" }
+      transitionTimingFunction: {
+        "stro": "cubic-bezier(.83,.16,.87,.65)",
+      },
     },
   },
   plugins: [require("daisyui")],
